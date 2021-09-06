0f9d045b642a67bd1f822bcadebd701b3fd040bb
David J. Malan <malan@harvard.edu>
2021-09-06 19:59:49 -0400

    tweaked meta

--- src/extension.ts
+++ src/extension.ts
@@ -35,7 +35,7 @@ class DDBViewProvider implements vscode.WebviewViewProvider {
 			<html lang="en">
 				<head>
 					<meta charset="UTF-8">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+					<meta name="viewport" content="initial-scale=1.0, width=device-width">
 					<link href="${styleUri}" rel="stylesheet">
 					<script src="${scriptUri}"></script>
 					<title>ddb50</title>
